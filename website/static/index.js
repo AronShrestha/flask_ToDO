c5728223e5e8a518e51c59203bbf776de72a4d6c
AronShrestha <aronshrestha498@gmail.com>
2023-03-06 22:27:02 +0545

    note-taking webapp

---
+++ website/static/index.js
@@ -0,0 +1,10 @@
+
+function deleteNote(noteId) {
+    console.log("pressed")
+    fetch("/delete-note", {
+      method: "POST",
+      body: JSON.stringify({ noteId: noteId }),
+    }).then((_res) => {
+      window.location.href = "/";
+    });
+  }
